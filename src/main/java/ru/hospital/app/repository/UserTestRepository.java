d0ec8187c20a62ab91a4ab7ac1571760cfad9577
dputiatinskii <dputiatinskii@neoflex.ru>
2021-10-29 12:15:39 +0300

    start springData

--- src/main/java/ru/hospital/app/repository/UserTestRepository.java
+++ src/main/java/ru/hospital/app/repository/UserTestRepository.java
@@ -3,11 +3,9 @@ package ru.hospital.app.repository;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.DigestUtils;
-import ru.hospital.app.model.Doctor;
 import ru.hospital.app.model.User;
 
 import java.nio.charset.StandardCharsets;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
